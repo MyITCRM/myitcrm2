071ede10e0d64ac8f5122f1f551e1cd868875ce6
geevpc <geevpc@gmail.com>
2010-07-13 22:09:49 +1000

    Just testing my linux intsall with git

--- db/migrate/002_create_users.rb
+++ db/migrate/002_create_users.rb
@@ -31,7 +31,8 @@ class CreateUsers < ActiveRecord::Migration
      @user = User.create :username => "admin",                        
                         :crypted_password => "1b9b040d838f59c16a843e9b49a8cd23a02c3945d27af80dfa68d208712d696a1cf22858cd91b76865869299aa3b4e2bd495cf97532168fddfdb20e59519aa35",
                         :password_salt => "3t3Xc_YhQ6AeTlUde-Om",
-                        :email => "admin@example.net"
+                        :email => "admin@example.net",
+                        :roles_mask => "1"
   end
 
 
