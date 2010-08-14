cd2b35763f67f86f71d5698e9ce5840825ae5b2c
geevpc <geevpc@gmail.com>
2010-08-14 23:09:35 +1000

    Updated some email validations and removed obsolete commented out text.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,4 +1,4 @@
 class Setting < ActiveRecord::Base
   validates_presence_of :address,:business_name,:city,:state,:zip,:phone,:email
-  validates_format_of  :email,:billing_email,:business_contact_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, 
+  validates_format_of  :email,:billing_email,:business_contact_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on =>  :create
 end
