0e6e41d202a5effe4b58454f8552cbc09d4833d2
geevpc <geevpc@gmail.com>
2010-12-08 18:54:18 +1100

    Added files in prepartion to installing RedCloth. Heaps of other changes, css styling, form layouts, etc....

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def sortable(column, title = nil)
-    title ||= column.titleize
+    title ||= column.titleize                       
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "ASC" ? "DESC" : "ASC"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
 
-end
+
+ def textiletoolbar_for(field_id)
+          javascript_include_tag('jstoolbar/jstoolbar') +
+            javascript_include_tag('jstoolbar/textile') +
+            javascript_include_tag("jstoolbar/lang/jstoolbar-en") +
+          javascript_tag("var textileToolbar = new jsToolBar($('#{field_id}'));  textileToolbar.draw();")
+        end
+        end
--- config/environment.rb
+++ config/environment.rb
@@ -17,10 +17,7 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-   config.gem "prawn", :lib => "prawn", :version => '0.8.4'
+    config.gem "prawn", :lib => "prawn"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
