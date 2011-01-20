c87cf33db3a2d0aa3efe283ecf506b31d7917411
geevpc <geevpc@gmail.com>
2011-01-21 02:10:23 +1100

    Updating to Rails 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,11 @@ class User < ActiveRecord::Base
 validates_presence_of :name, :address, :city, :username, :email, :phone, :state, :zip
 
 validates_format_of  :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  before_create :new_user
   
 
-named_scope :with_role, lambda { |role| {:conditions => "roles_mask & #{2**ROLES.index(role.to_s)} > 0"} }
+scope :with_role, lambda { |role| {:conditions => "roles_mask & #{2**ROLES.index(role.to_s)} > 0"} }
 
 # Roles_Mask values
   # Admin = 1
@@ -56,7 +58,8 @@ named_scope :with_role, lambda { |role| {:conditions => "roles_mask & #{2**ROLES
     roles.map(&:to_sym)
   end
 # Used to set New Users to default to active
-  def before_create
+
+  def new_user
     self.active ||= "1"
     self.roles_mask ||= "32"
     if self.created_by == nil
@@ -65,7 +68,7 @@ named_scope :with_role, lambda { |role| {:conditions => "roles_mask & #{2**ROLES
   end
  
   def self.search(search, page)
-      paginate :per_page => 20, :page => page,
+      User.paginate :per_page => 20, :page => page,
                           :conditions => ['name LIKE  ?', "%#{search}%"],
                           :order => 'id'
    end
--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,4 +1,27 @@
+# MyIT CRM - Repair's Business CRM Software
+# Copyright (C) 2009-2010  Glen Vanderhel
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
 class UserSession < Authlogic::Session::Base
   logout_on_timeout true
+
+def to_key
+   new_record? ? nil : [ self.send(self.class.primary_key) ]
+end
+
  
 end
