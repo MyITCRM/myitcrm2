5caaae0f4da5a6b538d909051322c1855cc5ff40
myitcrm <myitcrm@gmail.com>
2011-07-04 16:59:27 +1000

    Changed to CKEditor for textarea's

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -10,7 +10,7 @@ class WorkOrder < ActiveRecord::Base
 
   before_create :workorder_created
   before_update :workorder_updated, :change_assignment
-  before_save :lookup_assigned_username, :change_assignment
+  before_save :lookup_assigned_username, :change_assignment, :closed_action
 
 
   def workorder_created
@@ -33,16 +33,19 @@ class WorkOrder < ActiveRecord::Base
 
 # Used to obtain the Assigned Users name from the database on Work Order saving, instead of making a separate call each time it's displayed in the table
   def lookup_assigned_username
+    if self.assigned_to_username.blank? then
+      else
       if self.status_id == 1
         assigned_user = User.where(["name = ?", assigned_to_username]).first
         self.assigned_to_id = assigned_user.id
+      end
       end
   end
 
   def change_assignment
     #  This will change the status from NEW to Assigned when a user is assigned to a NEW Work Order
     if self.assigned_to_username.blank? then
-#    If there is no assigned user, it will change the status back to NEW
+#     If there is no assigned user, it will change the status back to NEW
       new_wo_status = "1"
       self.status_id = new_wo_status
     else
@@ -50,6 +53,12 @@ class WorkOrder < ActiveRecord::Base
         new_wo_status = "2"
         self.status_id = new_wo_status
       end
+    end
+  end
+  def closed_action
+    if self.closed == 1
+       closed_status = "6"
+      self.status_id = closed_status
     end
   end
 end
