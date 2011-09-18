b8749a31eb2f02010fc18c9163095c6a706fb2a5
myitcrm <myitcrm@gmail.com>
2011-09-19 08:12:33 +1000

    Navigation menu now works. Just need to do some more css work.

--- config/navigation.rb
+++ config/navigation.rb
@@ -25,30 +25,6 @@ SimpleNavigation::Configuration.run do |navigation|
     end
   end
   end
-  # Specify a custom renderer if needed.
-  # The default renderer is SimpleNavigation::Renderer::List which renders HTML lists.
-  # The renderer can also be specified as option in the render_navigation call.
-  # navigation.renderer = Your::Custom::Renderer
-
-  # Specify the class that will be applied to active navigation items. Defaults to 'selected'
-#  navigation.selected_class = 'your_selected_class'
-
-  # Specify the class that will be applied to the current leaf of
-  # active navigation items. Defaults to 'simple-navigation-active-leaf'
-  navigation.active_leaf_class = 'sub_menu'
-
-  # Item keys are normally added to list items as id.
-  # This setting turns that off
-  # navigation.autogenerate_item_ids = false
-
-  # You can override the default logic that is used to autogenerate the item ids.
-  # To do this, define a Proc which takes the key of the current item as argument.
-  # The example below would add a prefix to each key.
-  # navigation.id_generator = Proc.new {|key| "my-prefix-#{key}"}
-
-  # If you need to add custom html around item names, you can define a proc that will be called with the name you pass in to the navigation.
-  # The example below shows how to wrap items spans.
-  # navigation.name_generator = Proc.new {|name| "<span>#{name}</span>"}
 
   # The auto highlight feature is turned on by default.
   # This turns it off globally (for the whole plugin)
