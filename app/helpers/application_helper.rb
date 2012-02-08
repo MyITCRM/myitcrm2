c65a23ab6ff493d604014d54f6cdf02cd27ff9d9
Glen Vanderhel <glen@myitcrm.com>
2012-02-08 21:55:20 +1100

    Update JQuery and also refactoring out tables in invoice section and converting them into divs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,10 +61,12 @@ end
 
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, [new_object], :child_index => "new_#{association}") do |builder|
+    fields = f.simple_fields_for(association, new_object, :index => "new_#{association}") do |builder|
       render(association.to_s.singularize, :f => builder)
     end
     link_to_function(name, "add_fields(this, '#{association}', '#{escape_javascript(fields)}')" )
+    #link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+
   end
 
 end
