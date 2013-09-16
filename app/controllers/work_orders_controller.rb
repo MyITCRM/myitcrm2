1e34c15d0047bef2d5c7692df99ff239290b44f9
glenv <myitcrm@gmail.com>
2013-09-16 22:16:56 +1000

    Misc. fixes.

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -17,7 +17,7 @@
 
 class WorkOrdersController < ApplicationController
   before_filter :login_required # User must be logged in first
-  authorize_resource  # Used by CanCan to restrict controller access
+  authorize_resource # Used by CanCan to restrict controller access
   helper_method :sort_column, :sort_direction # Used for sorting columns
 
   def index
@@ -30,7 +30,7 @@ class WorkOrdersController < ApplicationController
       @re_opened_work_orders = WorkOrder.where("status_id = 5 AND user_id = ?", current_user.id)
       @closed_work_orders = WorkOrder.where("status_id = 6 AND user_id = ?", current_user.id).order("closed_date DESC")
     elsif can? :manage, WorkOrder
-      @new_work_orders = WorkOrder.where("status_id = 1" )
+      @new_work_orders = WorkOrder.where("status_id = 1")
       @assigned_work_orders = WorkOrder.where("status_id = 2")
       @on_hold_work_orders = WorkOrder.where("status_id = 3")
       @pending_work_orders = WorkOrder.where("status_id = 4")
@@ -53,19 +53,19 @@ class WorkOrdersController < ApplicationController
     @work_order = WorkOrder.new
     if can? :create, WorkOrder
       if params[:user_id].present? and current_user.client
-          @client = User.find(current_user.id)
-          @title = (t "workorder.creating_wo_for")+@client.name.camelcase
+        @client = User.find(current_user.id)
+        @title = (t "workorder.creating_wo_for")+@client.name.camelcase
       else
         if params[:user_id].present? and current_user.employee
           @client = User.find(params[:user_id])
           @title = (t "workorder.creating_wo_for")+@client.name.camelcase
         end
       end
-    if params[:user_id].blank? and current_user.employee
-      redirect_to clients_url
-      flash[:info] = "Please Select a Client first"
+      if params[:user_id].blank? and current_user.employee
+        redirect_to clients_url
+        flash[:info] = 'Please Select a Client first'
+      end
     end
-  end
   end
 
   def edit
@@ -96,7 +96,7 @@ class WorkOrdersController < ApplicationController
   end
 
   def update
-    @title = (t "workorder.t_workorders" ) + ' #' + params[:id].to_s
+    @title = (t "workorder.t_workorders") + ' #' + params[:id].to_s
     @work_order = WorkOrder.find(params[:id])
     @work_order.user_id = current_user.id if current_user.client
     @work_order.edited_by = current_user.username
@@ -104,55 +104,55 @@ class WorkOrdersController < ApplicationController
     invoicing_enabled = true
 
     if invoicing_enabled.present? and @work_order.closed
-       respond_to do |format|
-      if @work_order.update_attributes(params[:work_order])
-        flash[:notice] = 'Work Order was successfully updated.'
-        format.html { redirect_to new_work_order_invoice_path(:work_order_id => @work_order.id)  }
-        format.xml { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml { render :xml => @work_order.errors, :status => :unprocessable_entity }
-      end
+      respond_to do |format|
+        if @work_order.update_attributes(params[:work_order])
+          flash[:notice] = 'Work Order was successfully updated.'
+          format.html { redirect_to new_work_order_invoice_path(:work_order_id => @work_order.id) }
+          format.xml { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml { render :xml => @work_order.errors, :status => :unprocessable_entity }
+        end
       end
     else
-    respond_to do |format|
-      if @work_order.update_attributes(params[:work_order])
-        flash[:notice] = 'Work Order was successfully updated.'
-        format.html { redirect_to(@work_order) }
-        format.xml { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml { render :xml => @work_order.errors, :status => :unprocessable_entity }
-      end
+      respond_to do |format|
+        if @work_order.update_attributes(params[:work_order])
+          flash[:notice] = 'Work Order was successfully updated.'
+          format.html { redirect_to(@work_order) }
+          format.xml { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml { render :xml => @work_order.errors, :status => :unprocessable_entity }
+        end
       end
     end
   end
 
   def close
-    @title = (t "workorder.t_workorders" ) + ' #' + params[:id].to_s
+    @title = (t "workorder.t_workorders") + ' #' + params[:id].to_s
     @work_order = WorkOrder.find(params[:id])
     #@work_order.edited_by = current_user.username
     #@work_order.closed_by = current_user.username
     #@work_order.dynamic_attributes = [:status_id, :resolution, :assigned_to_username, :closed] if can? :manage, WorkOrder
     #invoicing_enabled = true
     if @work_order.assigned_to_id.blank?
-      redirect_to(@work_order) 
+      redirect_to(@work_order)
       flash[:alert] = "Work Order needs to be assigned to an employee first before closing."
       respond_to do |format|
-            if @work_order.update_attributes(params[:work_order])
-              flash[:notice] = 'Work Order was successfully closed.'
-              format.html { redirect_to(@work_order) }
-              format.xml { head :ok }
-            else
-              format.html { render :action => "close" }
-              format.xml { render :xml => @work_order.errors, :status => :unprocessable_entity }
-            end
-            end
-          end
+        if @work_order.update_attributes(params[:work_order])
+          flash[:notice] = 'Work Order was successfully closed.'
+          format.html { redirect_to(@work_order) }
+          format.xml { head :ok }
+        else
+          format.html { render :action => "close" }
+          format.xml { render :xml => @work_order.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
   end
 
   def assign
-    @title = (t "workorder.t_workorders" ) + ' #' + params[:id].to_s
+    @title = (t "workorder.t_workorders") + ' #' + params[:id].to_s
     @work_order = WorkOrder.find(params[:id])
 # Only change status if the status is either NEW or Assigned
     @status = @work_order.status_id = 2 if @work_order.status_id < 3
@@ -169,7 +169,7 @@ class WorkOrdersController < ApplicationController
   end
 
   def destroy
-    @title = (t "workorder.t_workorders" ) + ' #' + params[:id].to_s
+    @title = (t "workorder.t_workorders") + ' #' + params[:id].to_s
     @work_order = WorkOrder.find(params[:id])
     @work_order.destroy
 
@@ -180,13 +180,13 @@ class WorkOrdersController < ApplicationController
   end
 
 
-   private
+  private
 
   def sort_column
-   Product.column_names.include?(params[:sort]) ? params[:sort] : "id"
+    Product.column_names.include?(params[:sort]) ? params[:sort] : "id"
   end
 
   def sort_direction
-   %w[ASC DESC].include?(params[:direction]) ? params[:direction] : "ASC"
+    %w[ASC DESC].include?(params[:direction]) ? params[:direction] : "ASC"
   end
 end
